6479bca92780ad724b7e5a300b82e439b9434aee
JulioLaz <julioalazarte@gmail.com>
2022-08-07 20:47:56 -0300

    nuevos cambios

--- 001-angular/src/app/app.component.ts
+++ 001-angular/src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '001-angular';
-}
+  users: string[]= ['julio','juan', 'edu', 'gabi' , 'vivi', 'rodi'];
+
+
+saludo(){
+  alert('hola vivi')
+  }
+
+// deleteUser(user:any){
+//   for(let i = 0; i< this.users.length; i++){
+//     if (user === this.users[i]){
+//       this.users.splice(i, 1)
+//       }
+//     }
+//   }
+
+
+
+deleteUser(user:any){
+  alert(user+' eliminado')
+          this.users.splice(user, 1)
+      }
+    
+  }
+
+
+
--- 001-angular/src/app/app.module.ts
+++ 001-angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NuevoComponent } from './nuevo/nuevo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NuevoComponent
   ],
   imports: [
     BrowserModule,
